40660ae8926e687c798b7224a145189f3157316f
songjihoon12 <jjang1702@naver.com>
2024-05-02 17:46:45 +0900

    티케팅

--- src/song/Ticketing.java
+++ src/song/Ticketing.java
@@ -1,23 +1,35 @@
 package song;
 
 import chanhee.UserRepository;
-import yohanNew.Movie;
+import yohanNew.ReservationRepository;
+import yohanNew.ReservationView;
 import yohanNew.*;
-
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static util.SimpleInput.input;
+import static util.SimpleInput.stopInput;
+
 
 public class Ticketing {
     UserRepository ur = new UserRepository();
     ReservationRepository rr = new ReservationRepository();
+    ReservationView rv = new ReservationView();
+    private static ReservationRepository repository;
+
+    static {
+        repository = new ReservationRepository();
+
+    }
+
+
     public void menu() {
         boolean flag = checkPassword();
         if (!flag) {
             return;
         }
-        ticketingInfo();
+        viewReservationInfo();
     }
 
     private boolean checkPassword() {
@@ -33,8 +45,45 @@ public class Ticketing {
 
     }
 
-    private void ticketingInfo() {
+    private void viewReservationInfo() {
+        List<String> fgnasfa = new ArrayList<>();
 
 
-    }
+        List<ReservationInfo> currentUsersReservationList = repository.getReservationInfoList()
+                .stream()
+                .filter(reservationInfo -> reservationInfo.getMovieUser().equals(ur.getCurrentMovieUser()))
+                .collect(Collectors.toList());
+        if(currentUsersReservationList.size() == 0) {
+            repository.addReservationInfo(ReservationView.movieUser, "확인용1", "시간1", fgnasfa);
+            repository.addReservationInfo(ReservationView.movieUser, "확인용2", "시간2", fgnasfa);
+            repository.addReservationInfo(ReservationView.movieUser, "확인용3", "시간3", fgnasfa);
+
+            System.out.println("예매하신 영화가 없습니다.");
+            stopInput();
+            return;
+        }
+        while (true) {
+            int countNum = 1;
+            for (ReservationInfo reservationInfo : currentUsersReservationList) {
+                System.out.printf("%d %s %s\n", countNum++, reservationInfo.getMovie().getMovieName(),reservationInfo.getMovie().getTime());
+            }
+            System.out.println("0. 뒤로가기");
+            System.out.println("예약취소할 영화의 번호를 입력해 주세요");
+            int inputNumber = 0;
+            try {
+                inputNumber = Integer.parseInt(input(">>"));
+                if(inputNumber == 0) return;
+                System.out.printf("'%s'영화 예약이 취소되었습니다.\n", currentUsersReservationList.get(inputNumber - 1).getMovie().getMovieName());
+                repository.getReservationInfoList().remove(currentUsersReservationList.get(inputNumber - 1));
+                return;
+            } catch (NumberFormatException e) {
+                System.out.println("숫자로 입력해 주세요.");
+                stopInput();
+            } catch (IndexOutOfBoundsException e) {
+                System.out.println("정확한 번호를 입력해 주세요.");
+                stopInput();
+            }
+        }
+    };
+
 }
